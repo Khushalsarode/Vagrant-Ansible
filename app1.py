ef6c9d98757cfd8e4ed769d78462c0ddf1fd9603
Khushalsarode <khushalsarode.in@gmail.com>
2023-11-25 04:56:59 +0530

    first commit

---
+++ app1.py
@@ -0,0 +1,35 @@
+# app1.py
+from flask import Flask
+import pymysql
+
+app = Flask(__name__)
+
+# Database Configuration
+db_config = {
+    'user': 'app1_user',
+    'password': 'app1_password',
+    'host': 'database_host',
+    'database': 'db_one',
+}
+
+@app.route('/db')
+def db_api():
+    # Connect to MySQL
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+
+    # Perform a simple database query (replace with your actual query)
+    cursor.execute('SELECT * FROM your_table;')
+    result = cursor.fetchall()
+
+    # Close the connection
+    connection.close()
+
+    return str(result)
+
+@app.route('/non-db')
+def non_db_api():
+    return 'HTTP 200 OK'
+
+if __name__ == '__main__':
+    app.run(port=5000)
